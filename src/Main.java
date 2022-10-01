bf71ec775d002451729c3362f7f64745b6707867
Andrew Zola <and.zola.77@gmail.com>
2022-10-01 20:42:03 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int variable = 9;
+
+        variable = variable > 10 ? variable - 10 : variable + 10;
+
+        System.out.println(variable);
+    }
+
+   /* public static void main(String args[]) {
+        int variable = 7;
+        if (variable > 10) {
+            variable = variable - 10;
+        } else {
+            variable = variable + 10;
+        }
+        System.out.println(variable);
+    }*/
+}
